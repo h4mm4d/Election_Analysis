1f3541c030eed9df5752166284156d748320a342
Hammad Rahman <h4mm4d@gmail.com>
2021-11-21 10:29:55 -0600

    yes

--- Python_practice.py
+++ Python_practice.py
@@ -1,1 +1,16 @@
-Python_practice.py
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, 
+               {"county":"Denver", "registered_voters": 463353}, 
+               {"county":"Jefferson", "registered_voters": 432438}]
+
+
+'''for i in voting_data:
+    
+     print (i['county'])
+    
+    
+     print (f"{i['county']} has {i['registered_voters']} registred voters.")'''
+
+
+
+
+     
